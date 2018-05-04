0ba1294301a8bd49350c3982e29e5e53af9215e1
Federico Gimenez <federico@giantswarm.io>
2018-05-04 19:42:04 +0200

    implement check resource methods

--- integration/helm_test.go
+++ integration/helm_test.go
@@ -9,6 +9,14 @@ import (
 	"testing"
 
 	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/microerror"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	resourceNamespace = "kube-system"
+	labelSelector     = "app=node-exporter,kind=legacy"
 )
 
 var (
@@ -86,9 +94,99 @@ func TestMigration(t *testing.T) {
 }
 
 func checkResourcesInstalled() error {
+	c := f.K8sClient()
+	listOptions := metav1.ListOptions{
+		LabelSelector: labelSelector,
+	}
+
+	ds, err := c.Extensions().DaemonSets(resourceNamespace).List(listOptions)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	if len(ds.Items) != 1 {
+		return microerror.Newf("unexpected number of daemonsets, want 1, got %d", len(ds.Items))
+	}
+
+	r, err := c.Rbac().Roles(resourceNamespace).List(listOptions)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	if len(r.Items) != 1 {
+		return microerror.Newf("unexpected number of roles, want 1, got %d", len(r.Items))
+	}
+
+	rb, err := c.Rbac().RoleBindings(resourceNamespace).List(listOptions)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	if len(rb.Items) != 1 {
+		return microerror.Newf("unexpected number of rolebindings, want 1, got %d", len(rb.Items))
+	}
+
+	s, err := c.Core().Services(resourceNamespace).List(listOptions)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	if len(s.Items) != 1 {
+		return microerror.Newf("unexpected number of services, want 1, got %d", len(s.Items))
+	}
+
+	sa, err := c.Core().ServiceAccounts(resourceNamespace).List(listOptions)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	if len(sa.Items) != 1 {
+		return microerror.Newf("unexpected number of serviceaccountss, want 1, got %d", len(sa.Items))
+	}
+
 	return nil
 }
 
 func checkResourcesRemoved() error {
+	c := f.K8sClient()
+	listOptions := metav1.ListOptions{
+		LabelSelector: labelSelector,
+	}
+
+	ds, err := c.Extensions().DaemonSets(resourceNamespace).List(listOptions)
+	if err == nil && len(ds.Items) > 0 {
+		return microerror.New("expected error querying for daemonsets didn't happen")
+	}
+	if !apierrors.IsNotFound(err) {
+		return microerror.Mask(err)
+	}
+
+	r, err := c.Rbac().Roles(resourceNamespace).List(listOptions)
+	if err == nil && len(r.Items) > 0 {
+		return microerror.New("expected error querying for roles didn't happen")
+	}
+	if !apierrors.IsNotFound(err) {
+		return microerror.Mask(err)
+	}
+
+	rb, err := c.Rbac().RoleBindings(resourceNamespace).List(listOptions)
+	if err == nil && len(rb.Items) > 0 {
+		return microerror.New("expected error querying for rolebindings didn't happen")
+	}
+	if !apierrors.IsNotFound(err) {
+		return microerror.Mask(err)
+	}
+
+	s, err := c.Core().Services(resourceNamespace).List(listOptions)
+	if err == nil && len(s.Items) > 0 {
+		return microerror.New("expected error querying for services didn't happen")
+	}
+	if !apierrors.IsNotFound(err) {
+		return microerror.Mask(err)
+	}
+
+	sa, err := c.Core().ServiceAccounts(resourceNamespace).List(listOptions)
+	if err == nil && len(sa.Items) > 0 {
+		return microerror.New("expected error querying for serviceaccounts didn't happen")
+	}
+	if !apierrors.IsNotFound(err) {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
